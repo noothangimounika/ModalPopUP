0e57db241f47cea70ae146c38184b42f72cfd887
noothangimounika <noothangimounika@gmail.com>
2024-06-29 13:36:25 +0530

    Modal PopUp

---
+++ script.js
@@ -0,0 +1,19 @@
+const modal=document.querySelector(".modal"),
+    btn=document.getElementById("btn"),
+    close=document.querySelector(".close");
+
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+function openModal(e){
+    e.preventDefault();
+    modal.style.display="block";
+}
+
+function closeModal(){
+
+    modal.style.display="none";
+}
+
